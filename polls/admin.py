a7db1ac706707628aac08de6f83cd7e41b0c889d
elricgonza <elricgonza@gmail.com>
2018-07-10 16:17:49 -0700

    Add WIDGET -mapwidgets-  OK

--- polls/admin.py
+++ polls/admin.py
@@ -5,9 +5,14 @@ from django.contrib import admin
 from polls.models import *
 from django.contrib.admin import AdminSite
 import psycopg2
+from mapwidgets.widgets import GooglePointFieldWidget
+from django.contrib.gis.db import models
 
 @admin.register(Question)
 class Question(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        models.PointField: {"widget": GooglePointFieldWidget}
+    }
+    #pass
 
 # Register your models here.
