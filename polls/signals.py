a2f8b9ca2d39a4391ac8a512a7e6b0256ea726ad
elricgonza <elricgonza@gmail.com>
2018-07-09 17:59:22 -0700

    SIGNALS ok post_save, post_delete

--- polls/signals.py
+++ polls/signals.py
@@ -1,56 +1,47 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from django.conf import settings
 from polls.models import Question, Choice
-import os.path
 from django.db import connection
-
-from django.conf import settings
-from django.db.utils import ConnectionHandler
 
 
 @receiver(post_save, sender=Question)
-@receiver(post_save, sender=Choice)
 def model_post_save(sender, **kwargs):
-    print('Grabadez: {}'.format(kwargs['instance'].__dict__))
+    if kwargs.get('created', False):
+        action_is= 'new'
+    else:
+        action_is= 'update'
+
+    print('SAVED: {}'.format(kwargs['instance'].__dict__))
     print('******************************************')
-    print(kwargs['instance'].question_text)
+    print(kwargs['instance'].question_text) + '--->' +  action_is
     print('********************************************')
 
     with connection.cursor() as cursor:
-        cursor.execute(
-        "insert into question (id, question_text, usr) values (1, 'oso', 'I')"
-        )
-        cursor.commit()
-        cursor.close()
+        try:
+            sql = "insert into public.question (id, question_text, usr) values (%s, %s, %s)"
+            cursor.execute (sql, (
+                kwargs['instance'].id,
+                kwargs['instance'].question_text,
+                kwargs['instance'].id
+                )
+            )
+        except Exception as e:
+            print(type(e))
+            print(e.args)
+            print(e)
 
 
-    '''
-    private_connections = ConnectionHandler(settings.DATABASES)
-    #db = router.db_for_write(model)
-    new_conn = private_connections[db]
-    new_conn.enter_transaction_management()
-    new_conn.managed(True)
-    new_cur = new_conn.cursor()
-    new_cur.execute(
-        "insert into question (id, question_text, usr) values (1, 'oso', 'I')"
-    )
-    new_conn.commit()
-    new_conn.close()
-    '''
-
-
-    '''
-    cursor = connection.cursor()
-    #    "insert into question (id, question_text, usr) values (1, 'oso', 'I')"
-    #    "select question_text from polls_question where id=2"
-    cursor.execute(
-        "insert into question (id, question_text, usr) values (1, 'oso', 'I')"
-    )
-    #row = cursor.fetchone()
-    cursor.close
-    #print (row[0])
-    print ('-------new insert----')
-    '''
-
+@receiver(post_delete, sender=Question)
+def model_post_delete(sender, **kwargs):
+    print('******************************************')
+    print ("DELETED: %s" % str(kwargs['instance'].id) + '---' +  kwargs['instance'].question_text )
+    print('******************************************')
+    with connection.cursor() as cursor:
+        try:
+            sql = "delete from public.question where id = %s;"
+            cursor.execute (sql, (int(kwargs['instance'].id),))
+        except Exception as e:
+            print(type(e))
+            print(e.args)
+            print(e)
 
