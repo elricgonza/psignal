54d198023a779d71e6a9130c447164ad359fa7bd
elricgonza <elricgonza@gmail.com>
2018-07-13 18:42:28 -0700

    test CALLPROC err

--- polls/signals.py
+++ polls/signals.py
@@ -18,6 +18,7 @@ def model_post_save(sender, **kwargs):
 
     with connection.cursor() as cursor:
         try:
+            '''
             sql = "insert into public.question (id, question_text, usr) values (%s, %s, %s)"
             cursor.execute (sql, (
                 kwargs['instance'].id,
@@ -25,6 +26,59 @@ def model_post_save(sender, **kwargs):
                 kwargs['instance'].id
                 )
             )
+            '''
+            print('<<<kwargs')
+            print(kwargs['instance'].id)
+            print(kwargs['instance'].question_text)
+            print('>>>')
+            #cursor.callproc('f_question', [kwargs['instance'].id])
+
+            sql = 'select * from f_question(%s)'
+            some_id= 16
+            #cursor.execute(sql, ([16]))  #no invoca funciones?
+            #cursor.callproc('f_question', [16])
+            '''
+create or replace function f_question(
+    id int
+    ) returns table (
+        id int,
+            question_text varchar(50),
+                usr int
+                ) language sql as $$
+                    select id, question_text, id*2 from question where id=$1;
+                    $$;
+
+                    --select * from f_question(16);
+
+            '''
+
+
+            cursor.execute("select * from question;")
+            results = cursor.fetchall()
+            while True:
+                if not results:
+                    print '---NO hay ROWS---'
+                    break
+                for result in results:
+                    print 'hay ROW'
+                    break
+            print('////////ret')
+            print(results)
+            #print(cursor.fetchone()[0])
+            #print(cursor.fetchone()[1])
+            #print(cursor.fetchone()[2])
+            print('////////')
+
+            '''
+            sql = "insert into public.question (id, question_text, usr) values (%s, %s, %s)"
+            cursor.execute (sql, (
+                results.id,
+                results.question_text,
+                results.id
+                )
+            )
+            '''
+
         except Exception as e:
             print(type(e))
             print(e.args)
