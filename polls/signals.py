3a2d723055f6dbf2f7dd1aaded2c5b9b026ca8c2
elricgonza <elricgonza@gmail.com>
2018-07-31 18:10:43 -0700

    Corregido CALLPROC - post_save OK

--- polls/signals.py
+++ polls/signals.py
@@ -19,6 +19,7 @@ def model_post_save(sender, **kwargs):
     with connection.cursor() as cursor:
         try:
             '''
+            # inserta valores directamente
             sql = "insert into public.question (id, question_text, usr) values (%s, %s, %s)"
             cursor.execute (sql, (
                 kwargs['instance'].id,
@@ -27,57 +28,23 @@ def model_post_save(sender, **kwargs):
                 )
             )
             '''
-            print('<<<kwargs')
-            print(kwargs['instance'].id)
-            print(kwargs['instance'].question_text)
-            print('>>>')
-            #cursor.callproc('f_question', [kwargs['instance'].id])
-
-            sql = 'select * from f_question(%s)'
-            some_id= 16
-            #cursor.execute(sql, ([16]))  #no invoca funciones?
-            #cursor.callproc('f_question', [16])
-            '''#last
-create or replace function f_question(
-    id int
-    ) returns table (
-        id int,
-            question_text varchar(50),
-                usr int
-                ) language sql as $$
-                    select id, question_text, id*2 from question where id=$1;
-                    $$;
-
-                    --select * from f_question(16);
-
-            '''
-
-
-            cursor.execute("select * from question;")
-            results = cursor.fetchall()
-            while True:
-                if not results:
-                    print '---NO hay ROWS---'
-                    break
-                for result in results:
-                    print 'hay ROW'
-                    break
-            print('////////ret')
+            # get valores from f
+            cursor.callproc('f_question', [kwargs['instance'].id])
+            results = cursor.fetchone()
             print(results)
-            #print(cursor.fetchone()[0])
-            #print(cursor.fetchone()[1])
-            #print(cursor.fetchone()[2])
-            print('////////')
+            print('===========')
+            print(results[0])
+            print(results[1])
+            print(results[2])
+            print('===========')
 
-            '''
             sql = "insert into public.question (id, question_text, usr) values (%s, %s, %s)"
             cursor.execute (sql, (
-                results.id,
-                results.question_text,
-                results.id
+                results[0],
+                results[1],
+                results[2]
                 )
             )
-            '''
 
         except Exception as e:
             print(type(e))
